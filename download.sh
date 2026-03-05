fe2c4673319690a4858fb78064593c528d7b3e31
ymatzkevich <120640912+ymatzkevich@users.noreply.github.com>
2026-03-05 19:05:48 +0100

    add download script

---
+++ download.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+TRACKS_DIR="tracks"
+LOGS_DIR="logs"
+ARCHIVE_DIR="archive"
+
+mkdir -p "$TRACKS_DIR" "$LOGS_DIR" "$ARCHIVE_DIR"
+
+ARCHIVE_FILE="archive/archive.txt"
+
+touch "$ARCHIVE_FILE"
+
+if [ -z "${SOUNDCLOUD_URL:-}" ]; then
+  echo "ERROR: the playlist URL is not set! exiting... bye!"
+else
+  echo "starting download from: $SOUNDCLOUD_URL"
+  scdl \
+  -l "$SOUNDCLOUD_URL" \
+  --path "$TRACKS_DIR" \
+  --download-archive "$ARCHIVE_FILE" \
+  --name-format "%(artist)s - %(title)s" \
+  --original-art \
+  --onlymp3 \
+  --hidewarnings \
+  2>&1 | while IFS= read -r line; do
+   echo "$line"
+   if [[ "$line" == *"has already been recorded in the archive"* ]]; then
+     pkill -P $$ scdl
+     break
+   fi
+  done
+fi
